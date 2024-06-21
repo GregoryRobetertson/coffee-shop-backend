01fce577886cc78607bf24bb876e802e9ed43cb0
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-06-21 00:03:11 +0000

    updated

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,27 @@
-const { register, login, getUsers } = require('../controllers/userController');
-const auth = require('../middleware/auth');
-const { checkEmail, checkPassword, signToken } = require('../middleware/login');
-const role = require('../middleware/role');
-
-const userRouter = require('express').Router();
-
-userRouter.route('/register').post(register);
-
-userRouter.route('/login').post(checkEmail, checkPassword, signToken, login);
-
-userRouter.route('/').get(auth, role(['admin']), getUsers);
-
-module.exports = userRouter;
+const {
+    register,
+    login,
+    getUsers,
+    deleteOneUser,
+    getOneUser,
+    updateOneUser,
+  } = require('../controllers/userController');
+  const auth = require('../middleware/auth');
+  const { checkEmail, checkPassword, signToken } = require('../middleware/login');
+  const role = require('../middleware/role');
+  
+  const userRouter = require('express').Router();
+  
+  userRouter.route('/register').post(register);
+  
+  userRouter.route('/login').post(checkEmail, checkPassword, signToken, login);
+  
+  userRouter.route('/').get(auth, role(['admin']), getUsers);
+  
+  userRouter
+    .route('/:userId')
+    .get(auth, role(['user', 'admin']), getOneUser)
+    .delete(auth, role(['admin']), deleteOneUser)
+    .put(auth, role(['user', 'admin']), updateOneUser);
+  
+  module.exports = userRouter;
