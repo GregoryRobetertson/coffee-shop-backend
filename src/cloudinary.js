f1b59b464e24389f2c82ae7a98d81410d620ca4c
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-06-21 00:03:00 +0000

    created cloudinary config

---
+++ src/cloudinary.js
@@ -0,0 +1,17 @@
+const cloudinary = require('cloudinary').vs
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+cloudinary.config({
+cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+cloud_key: process.env.CLOUDINARY_KEY,
+cloud_secret: process.env.CLOUDINARY_SECRET
+})
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'coffee',
+        allowedFormats:[ 'jpg', 'png', 'jpeg']
+    }
+  });
+
+  module.exports = {cloudinary, storage};
