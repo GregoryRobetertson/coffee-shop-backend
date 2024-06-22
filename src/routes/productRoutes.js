ee45ed4a2400bdffc451c07b04cdcbbe638b862f
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-06-22 00:45:51 +0000

    debugged

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,12 +1,26 @@
 'use strict';
 
-const { getProducts, newProduct, getProductById } = require('../controllers/productController');
+const { admin, user, users } = require('../ACL');
+const {
+  newProduct,
+  getByCategory,
+  getProductById,
+  deleteProductById,
+  updateProductById,
+} = require('../controllers/productController');
+const role = require('../middleware/role');
+const upload = require('../middleware/upload');
 
 const productRouter = require('express').Router();
 
-productRouter.route('/').post(newProduct).get(getProducts);
+productRouter.route('/').post(upload.single('image'),newProduct).get(getByCategory);
 
-
-productRouter.route('/:id').get(getProductById)
-
+productRouter
+  .route('/:id')
+  .get(getProductById)
+  .delete(role(admin), deleteProductById)
+  .put(role(admin), updateProductById);
+productRouter
+  .route('/:id/purchase')
+  .put(role(user), (req, res) => res.send('Future Feature!'));
 module.exports = productRouter;
